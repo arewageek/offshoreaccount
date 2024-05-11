2c6ddc6be7115ff79e38e208c1430e39e4e50033
arewageek <arewageek@gmail.com>
2024-05-11 20:16:26 +0100

    Admin fixed

--- src/components/user/MobileNav.tsx
+++ src/components/user/MobileNav.tsx
@@ -1,11 +1,11 @@
 import AdminNavLinks from "../admin/AdminNavLinks";
 import NavLinks from "./NavLinks";
 
-export const MobileNav = (type = "user") => {
+export const MobileNav = ({ format }: { format: string }) => {
   return (
     <div className="w-[250pt] md:w-[15%] px-5 block left-5 py-5">
       <div className="w-full h-[80%] flex flex-col justify-between text-sm">
-        {type == "user" ? <NavLinks /> : <AdminNavLinks />}
+        {format == "user" ? <NavLinks /> : <AdminNavLinks />}
       </div>
     </div>
   );
