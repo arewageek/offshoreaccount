26bf236828fcff807a69fbc1fa9e640e9da6a8dc
Austin Ameh <arewageek@gmail.com>
2024-05-23 21:29:00 +0100

    Deployment Fixes: User balance errors

--- src/components/admin/UserBalance.tsx
+++ src/components/admin/UserBalance.tsx
@@ -5,20 +5,14 @@ export const UserBalance = async ({id}:{id: string | undefined}) => {
 
     const balance = await userBalance({id})
     
-    return (
-        <div className='w-full py-3'>
+    return <div className='w-full py-3'>
             <div className='text-xl font bold'>
             <h3>Account Balance</h3>
             <h3>
-<<<<<<< HEAD
-                ${ balance.toStrngLocale() }
-=======
                 ${ balance?.toLocaleString() }
->>>>>>> dev
             </h3>
             </div>
         </div>
-    )
 }
 
 export default UserBalance
